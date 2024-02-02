1df98166bfe15fa59ca4006f218ec48a976d90f0
Md Toriqul Islam <137867238+TheToriqul@users.noreply.github.com>
2024-02-03 01:28:54 +0800

    Update two-ns-connect-using-veth.sh

---
+++ two-ns-connect-using-veth.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Create network namespaces
+sudo ip netns add red
+sudo ip netns add blue
+
+# Create veth pair
+sudo ip link add tap1 type veth peer name tap2
+
+# Move interfaces to namespaces
+sudo ip link set tap1 netns red
+sudo ip link set tap2 netns blue
+
+# Bring up interfaces
+sudo ip netns exec red ip link set dev tap1 up
+sudo ip netns exec blue ip link set dev tap2 up
+
+# Assign IP addresses (adjust as needed)
+sudo ip netns exec red ip addr add 192.168.1.1/24 dev tap1
+sudo ip netns exec blue ip addr add 192.168.1.2/24 dev tap2
+
+# Print confirmation message
+sudo echo "Namespaces and veth pair created successfully!"
+
+# Optional: Test connectivity (uncomment if desired)
+sudo ip netns exec red ping 192.168.1.2
+sudo ip netns exec blue ping 192.168.1.1
+
+# Clean up (uncomment if you want to remove the namespaces)
+# sudo ip netns del red
+# sudo ip netns del blue
